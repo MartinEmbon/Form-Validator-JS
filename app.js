6504fcb4efb77fe4f4e8682a17e3e2d79eed6ed1
Martin <martinembon@hotmail.com>
2021-09-16 19:00:21 -0300

    SweetFlash

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const password = document.getElementById("password")
 const confPassword = document.getElementById("conf-password")
 const form = document.getElementById("form")
 const errorElement=document.getElementById("error")
-const edad = document.getElementById("edad")
-
+//const edad = document.getElementById("edad")
 form.addEventListener("submit",(e)=>{
     let mensaje = []
     if (fname.value === "" || fname.value==null){
@@ -26,16 +25,21 @@ form.addEventListener("submit",(e)=>{
         mensaje.push("Ambas senhas deven coincidir")
     }
 
-    else if (!edad.checked){
-        mensaje.push("Tem quer ser maior de 18 para acceder")
-    }
+    // else if (!edad.checked){
+    //     mensaje.push("Tem quer ser maior de 18 para acceder")
+    // }
     
     if (mensaje.length>0){
         e.preventDefault()
         errorElement.innerText = mensaje.join(", ")
    
     } else {
-        alert("Cadastrado com sucesso!!")
+        Swal.fire(
+            'The Internet?',
+            'That thing is still around?',
+            'question'
+          )
+          window.location.reload(true);
     }
 })
 
